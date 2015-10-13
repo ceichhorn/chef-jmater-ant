7d30793e55281e6fa25227d63c46ca0dbdcabfb2
Carl Eichhorn <ceichhorn@gmti.gannett.com>
2015-10-13 15:00:36 -0400

    GDPHAR-1410 Create Jmeter cookbook for performance testing

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 require 'fauxhai'
 
 
-describe 'jmeter-ant' do
+describe 'jmeter' do
   let(:chef_run) do
     ChefSpec::SoloRunner.new do |node|
       node.set['opsmatic']['integration_token'] = 'SomeToken'
@@ -13,7 +13,7 @@ describe 'jmeter-ant' do
   end
 
  it 'includes the `jmeter` recipe' do
-    expect(chef_run).to include_recipe('jmeter-ant')
+    expect(chef_run).to include_recipe('jmeter')
   end
 
   it 'creates templates directory' do
